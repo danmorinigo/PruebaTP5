22541abec44370d50f33942991c49ba1a17b760e
danmorinigo <dany.relojero@gmail.com>
2020-08-18 01:53:00 -0300

    Algunas pre y post

--- Arista.h
+++ Arista.h
@@ -12,15 +12,42 @@ private:
     int precio;
     double horasVuelo;
 public:
+
+    //PRE:  objeto destino debe estar creado
+    //POST: Crea un objeto Arista, atributo sig apuntando a Null, destino apunta al vertice pasado
+    //      como parametro, precio vale -1 y horasVuelo vale -1.0.
     Arista(Vertice* destino);
+
+    //PRE:  --
+    //POST: --
     ~Arista();
+
+    //PRE:  sig puede ser tanto un objeto Arista ya creado o null.
+    //POST: sig apunta al valor pasado por parametro.
     void asignarSiguiente(Arista* sig);
+
+    //PRE:  --
+    //POST: a precio se le asigna el valor pasado por parametro.
     void asignarPrecio(int precio);
+
+    //PRE:  --
+    //POST: a horasVuelo se le asigna el valor pasado por parametro.
     void asignarHoras(double horasVuelo);
 
+    //PRE:  --
+    //POST: Devuelve el valor guardado en sig.
     Arista* consultarSiguiente();
+
+    //PRE:  --
+    //POST: Devuelve Vertice* al cual si dirije la arista.
     Vertice* ConsultarDestino();
+
+    //PRE:  --
+    //POST: Devuelve valor almacenado en precio.
     int consultarPrecio();
+
+    //PRE:  --
+    //POST: Devuelve valor almacenado en horasVuelo.
     double consultarHoras();
 
 };
--- colaPrioridad.h
+++ colaPrioridad.h
@@ -10,23 +10,72 @@ private:
     NodoColaPrioridad* ultimo;
     int criterioOrden;//1-> pesoEntero / 2-> pesoDouble
 public:
+    //PRE:  criterioOrden debe ser valer 1 o 2
+    //POST: Crea objeto indicandole cual sera el criterio por el cual hará las comparaciones al ordenar
     ColaPrioridad(int criterioOrden);
+
+    //PRE:  --
+    //POST: Devuelve true si primero apunta a null, caso contrario devuelve false.
     bool vacia();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Devuelve Vertice* dentro del NodoColaPrioridad y elimina dicho nodo.
     Vertice* topAndPop();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Devuelve Vertice* dentro del NodoColaPrioridad.
     Vertice* topVertice();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Devuelve la prioridad actual de la cola.
     int topPrioridad();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Devuelve pesoEntero de dentro del NodoColaPrioridad.
     int topPrecio();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Devuelve pesoDouble de dentro del NodoColaPrioridad.
     double topHoras();
+
+    //PRE:  Cola no debe estar vacía.
+    //POST: Elimina primer nodo.
     void pop();
+
+    //PRE:  Objeto evaluado debe estar creado, la iteracion no puede ser menor a la actualmente activa.
+    //POST: Inserta dentro de la cola el Vertice* en el orden que le corresponde.
     void push(Vertice* evaluado, int precio, double horas, int iteracion);
+
+    //PRE:  --
+    //POST: Libera recursos utilizados.
     ~ColaPrioridad();
 private:
+    //PRE:  --
+    //POST: Inserta al final de la cola.
     void insertarAlFinal(NodoColaPrioridad* aInsertar);
+
+    //PRE:  --
+    //POST: El nodo se inserta delante del que estaba primero en la prioridad actual.
     void insertarNuevoEnPrioridad(NodoColaPrioridad* aInsertar);
+
+    //PRE:  --
+    //POST: El nodo se inserta ordenandolo dentro de la actual prioridad.
     void insertarOrdenado(NodoColaPrioridad* aInsertar);
+
+    //PRE:  --
+    //POST: primeroSegunPrioridad apuntara a valor pasado como parametro.
     void actualizarPrioridad(NodoColaPrioridad* aInsertar);
+
+    //PRE:  --
+    //POST: Devuelve true si criterioOrden vale 1, sino devuelve false.
     bool ordenPorPrecio();
+
+    //PRE:  --
+    //POST: Devuelve true si criterioOrden vale 2, sino devuelve false.
     bool ordenPorHoras();
+
+    //PRE:  --
+    //POST: Se encarga de insertar el nodo cuando la cola ya tiene elementos cargados.
     void insertarElemento(NodoColaPrioridad* aInsertar);
 };
 
