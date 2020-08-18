524a6d825af0781739598242e5d72648e1d525c8
danmorinigo <dany.relojero@gmail.com>
2020-08-18 09:17:15 -0300

    Pre y Post

--- etiqueta.h
+++ etiqueta.h
@@ -18,15 +18,41 @@ public:
     Etiqueta(Vertice* actual);
     ~Etiqueta();
 
+    //PRE:  Vertice* debe estac creado
+    //POST: Agrega Vertice* a la lista de Vertices* anterior.
+    //      Si anteriormente habia sido agregado se descarta.
     void sumoAnterior(Vertice* anterior);
+
+    //PRE:  --
+    //POST: pesoAcumulado valdra el valor peso pasado como parametro.
     void setPesoAcumulado(int peso);
+
+    //PRE:  --
+    //POST: pesoDouble valdra el valor pesoDouble pasado como parametro.
     void setPesoDouble(double pesoDouble);
+
+    //PRE:  --
+    //POST: iteracion valdra el valor iteracion pasado como parametro.
     void setIteracion(int iteracion);
 
+    //PRE:  --
+    //POST: Devuelve el valor almacenado en Vertice
     Vertice* getVertice();
+
+    //PRE:  --
+    //POST: Devuelve lista de antecesores del Vertice*
     list<Vertice*> getAnterior();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en pesoAcumulado.
     int getPesoAcumulado();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en pesoDouble.
     double getPesoDouble();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en iteracion.
     int getIteracion();
 };
 
--- nodoColaPrioridad.h
+++ nodoColaPrioridad.h
@@ -11,13 +11,36 @@ private:
     double pesoDouble;
     NodoColaPrioridad* siguiente;
 public:
+    //PRE:  Vertice* debe estar creado.
+    //POST: Crea el nodo con los valores pasados como parametro.
     NodoColaPrioridad(Vertice* vertice, int prioridad, int pesoEntero, double pesoDouble);
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en vertice.
     Vertice* obtenerVertice();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en prioridad.
     int getPrioridad();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en pesoEntero.
     int getEntero();
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en pesoDouble.
     double getDouble();
+
+    //PRE:  --
+    //POST: Cambia el valor de siguiente por el pasado como parametro.
     void setSiguiente(NodoColaPrioridad* siguiente);
+
+    //PRE:  --
+    //POST: Devuelve valor guardado en siguiente.
     NodoColaPrioridad* getSiguiente();
+
+    //PRE:  --
+    //POST: --
     ~NodoColaPrioridad();
 };
 #endif // NODOCOLAPRIORIDAD_H_INCLUDED
